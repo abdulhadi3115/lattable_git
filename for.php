b84358050f24df655f8d71d1b9c40977db802577
abdulhadi3115 <abhad9213@gmail.com>
2019-03-19 12:23:46 +0700

    baru

---
+++ for.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<!-- <?php
+for ($x = 0; $x <= 11; $x++){
+	echo "<h2>nomornya adalah : $x <br></h2>";
+}
+
+?> -->
+
+<?php
+for($i = 0; $i < 10; $i++){
+    echo "<h2>$i. abdul hadi<br></h2>";
+
+    
+}
+?>
+
+</body>
+</html>
---
+++ table.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <table>
+  <tr>
+    <th>Bulan</th>
+    <th>Tabungan</th>
+  </tr>
+  <?php  for($i = 0; $i < 10; $i++){ ?>
+      <tr>
+    <td>Januari</td>
+    <td>Rp 100.000,-</td>
+  </tr>
+
+  <?php
+  }
+  ?> 
+
+  <tr>
+    <td>Februari</td>
+    <td>Rp 200.000,-</td>
+  </tr>
+
+</table>
+
+
+</body>
+</html>
